7b6e38954beffc0f0ab9f2802662bf3e5155a44d
Claudiu Ionita <ionut5002@gmail.com>
2018-12-12 16:17:27 +0000

    licences ref no added and filter

--- env.js
+++ env.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 
 if (fs.existsSync('./public')) {
   process.env.NODE_ENV = 'production';
-  process.env.databaseUri = 'mongodb://rnce2018:ionut5002@ds219000.mlab.com:19000/test-rnce'; // Databse URI and database name
+  process.env.databaseUri = 'mongodb://rnce2018:ionut5002@ds219318.mlab.com:19318/rnce'; // Databse URI and database name
   process.env.databaseName = 'production database: rnce'; // Database name
 } else {
   process.env.NODE_ENV = 'development';
-  process.env.databaseUri = 'mongodb://rnce2018:ionut5002@ds219000.mlab.com:19000/test-rnce'; // Databse URI and database name
+  process.env.databaseUri = 'mongodb://rnce2018:ionut5002@ds219318.mlab.com:19318/rnce'; // Databse URI and database name
   process.env.databaseName = 'production database: rnce'; // Database name
 }
 
